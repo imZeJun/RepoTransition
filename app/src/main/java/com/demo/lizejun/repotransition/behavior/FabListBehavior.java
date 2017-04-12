883eca6fbdd6566480c9f54ced0ce536add3664e
lizejun <278618939@qq.com>
2017-04-13 00:44:52 +0800

    添加Fab滚动例子

--- app/src/main/java/com/demo/lizejun/repotransition/behavior/FabListBehavior.java
+++ app/src/main/java/com/demo/lizejun/repotransition/behavior/FabListBehavior.java
@@ -1,13 +1,16 @@
 package com.demo.lizejun.repotransition.behavior;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 public class FabListBehavior extends FloatingActionButton.Behavior {
+
+    private static final int MIN_CHANGED_DISTANCE = 30;
 
     public FabListBehavior(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -21,12 +24,25 @@ public class FabListBehavior extends FloatingActionButton.Behavior {
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        Log.d("FabListBehavior", "dyConsume=" + dyConsumed + ",tag=" + target.getTag());
-        if (dyConsumed > 0) {
-            child.hide();
-        } else if (dyConsumed < 0) {
-            child.show();
+        if (dyConsumed > MIN_CHANGED_DISTANCE) {
+            createValueAnimator(coordinatorLayout, child, false).start();
+        } else if (dyConsumed < -MIN_CHANGED_DISTANCE) {
+            createValueAnimator(coordinatorLayout, child, true).start();
         }
+    }
+
+    private Animator createValueAnimator(CoordinatorLayout coordinatorLayout, final View fab, boolean dismiss) {
+        int distanceToDismiss = coordinatorLayout.getBottom() - fab.getBottom() + fab.getHeight();
+        int end = dismiss ? 0 : distanceToDismiss;
+        float start = fab.getTranslationY();
+        ValueAnimator animator = ValueAnimator.ofFloat(start, end);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                fab.setTranslationY((Float) animation.getAnimatedValue());
+            }
+        });
+        return animator;
     }
 
 }
